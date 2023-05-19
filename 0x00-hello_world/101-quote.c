bbb055159486f267bbc269c804b1aacec9fe839f
fannyjoe <alabioyinlola@gmail.com>
2023-05-18 18:15:57 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * main - entry point.
  *
- * Return: always 0.
+ * Return: on success 1.
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 55);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
 	return(1);
 }
