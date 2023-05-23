048e633cd37bba27a536bb31a32815c30ec2a245
fannyjoe <alabioyinlola@gmail.com>
2023-05-23 15:36:12 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lower.
+ *
+ * @c: the character to check.
+ * Return: 1 on success, 0 otherwise
+ */
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (c == 'a')
+		{
+		_putchar('a');
+		_putchar('H');
+		}
+	_putchar('\n');
+	return (0);
+}
